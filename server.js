b01ee8f4af41534e0706848f8cc79798108c7fdd
cike8899 <18267850322.com>
2016-08-04 23:54:48 +0800

    Add query user detail ,create user ,remove user api.

--- server.js
+++ server.js
@@ -9,6 +9,49 @@ app.get('/listUsers', function (req, res) {
     });
 })
 
+//添加的新用户数据
+var user = {
+    "user4": {
+        "name": "mohit",
+        "password": "password4",
+        "profession": "teacher",
+        "id": 4
+    }
+}
+
+app.get('/addUser', function (req, res) {
+    // 读取已存在的数据
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        data["user4"] = user["user4"];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+})
+
+app.get('/:id', function (req, res) {
+    // 首先我们读取已存在的用户
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        var user = data["user" + req.params.id]
+        console.log(user);
+        res.end(JSON.stringify(user));
+    });
+})
+
+
+app.get('/deleteUser/:id', function (req, res) {
+
+    // First read existing users.
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        delete data["user" + req.params.id];
+
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+})
+
 var server = app.listen(8081, function () {
 
     var host = server.address().address
