3c30bec4f90d80f1c8204cf96bb8a24b46c77930
cike8899 <18267850322.com>
2016-08-07 19:09:15 +0800

    Add typings to this project

--- server.js
+++ server.js
@@ -58,5 +58,4 @@ var server = app.listen(8081, function () {
     var port = server.address().port
 
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
 })
---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/body-parser/index.d.ts" />
+/// <reference path="globals/cookie-parser/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/multer/index.d.ts" />
